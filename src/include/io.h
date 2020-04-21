b5b1e48383d54ae45191af01d84c6257472de5ec
Manolis Pitsikalis <manospits@iit.demokritos.gr>
2020-04-21 13:30:35 +0100

    added some comments in io.h

--- src/include/io.h
+++ src/include/io.h
@@ -7,25 +7,27 @@
 #define DATA_ARRAY_INITIAL_SIZE 128
 
 typedef struct attribute{
-    char name[ATTRIBUTE_LEN];
-    size_t size;
-    int (*cmp)(const void*,const void *);
-    char dtype;//d or s
-    size_t offset;
+    char name[ATTRIBUTE_LEN];               //name of the field
+    size_t size;                            //size of the values of the field
+    int (*cmp)(const void*,const void *);   //comparator to use (maybe useful)
+    char dtype;                             // type identifier for printing and memcpy  d (double) or s (string)
+    size_t offset;                          //offset of the field value in data row
 }attribute;
 
 typedef struct dataset_{
-    char **data;
-    attribute *attributes;
-    int attributes_number;
-    size_t row_size;
-    int rows;
-    int allocated_rows;
+    char **data;                            //char (byte) 2d dynamically allocated array for storing data
+    attribute *attributes;                  //array of attribute structs for field information
+    int attributes_number;                  //number of columns
+    size_t row_size;                        //size to allocate for each row in data array
+    int rows;                               //number of rows in data array with actual data
+    int allocated_rows;                     //allocated rows
 }dataset_;
 
 typedef struct dataset_ * dataset;
 
 void initialize_attribute(attribute * attr, char * name, size_t size, int (*cmp)(const void*,const void *), char dtype, size_t offset);
+/* initialize an attribute structure
+ * !!! it doesn't allocate the memory it just sets the values */
 
 dataset read_dataset(const char * path, const char * sep, int header, char ** string_fields, int cs);
 /* input: path is the location of the dataset
@@ -41,9 +43,24 @@ void free_dataset(dataset dt);
  */
 
 char ** get_row(dataset dt, int row);
+/*
+ * returns a pointer to the row with number `row'
+ */
+
 char * get_element(dataset dt, int row, int col);
+/*
+ * returns the address of the element in row col
+ */
+
 char * get_element_by_col_name(dataset dt, int row, char* name);
+/*
+ * returns the adress of element specified by row and field name
+ */
 
 int in_array(char *string, char **array, int s);
+/*
+ * checks if string is in array (0 False)
+ */
+
 
 #endif
