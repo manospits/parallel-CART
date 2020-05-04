3546bca356ff301b912432cf62bfcf4918fe8db7
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2020-05-04 22:08:32 +0100

    gathering votes

--- mpi/main.c
+++ mpi/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 #include "io/include/io.h"
 #include "tree/include/tree.h"
@@ -23,7 +24,7 @@ int main(void){
     Dataset test_set;
     Dataset train_subset;
     Tree clf_tree;
-    char **tree_votes;
+    char **forest_votes;
     char **forest_predictions;
     int n_ranks, rank;
 
@@ -38,19 +39,53 @@ int main(void){
     printf("[%d] Creating test, train subset...\n", rank);
     train_test_split(dt, &train_set, &test_set, test_train_ratio);
 
+    printf("[%d] %d\n", rank, dt->rows);
+    printf("[%d] train %d test %d\n", rank, train_set->rows, test_set->rows);
+
     printf("[%d] Creating randomly sampled subset...\n", rank);
-    create_random_subset(train_set, train_subset, train_set->rows, sample_ratio, rank);
+    create_random_subset(train_set, &train_subset, train_set->rows, sample_ratio, rank);
     printf("[%d] Finito %d\n", rank, train_subset->rows);
 
-    printf("Watering tree...\n");
+    printf("[%d] Watering tree...\n", rank);
     clf_tree = build_classification_tree(train_subset, "Gender", max_tree_height);
-    printf("Tree grew up...\n");
+    printf("[%d] Tree grew up...\n", rank);
+
+    // Allocate memory for tree votes
+    char **tree_votes = malloc(sizeof(char*) * test_set->rows);
+
+    for(int i=0; i < test_set->rows; i++){
+        tree_votes[i] = malloc(sizeof(char) * STRING_SIZE);
+    }
+
+    printf("[%d] Gathering seeds...\n", rank);
+    for(int i = 0; i < test_set->rows; i++) {
+        strcpy(tree_votes[i], predict_row(clf_tree, test_set->data[i]));
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Allocate memory for forest votes
+    if(rank == 0) {
+        char **forest_votes = malloc(sizeof(char*) * n_ranks);
+
+        for(int i=0; i < n_ranks; i++){
+            forest_votes[i] = malloc(sizeof(char) * test_set->rows);
+        }
+    }
+
+    printf("[%d] Sending votes\n", rank);
+
+    MPI_Gather(&tree_votes, STRING_SIZE * test_set->rows, MPI_CHAR, &forest_votes, STRING_SIZE * test_set->rows, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    printf("Chopping the tree :\\\n");
+    del_tree(clf_tree);
 
     printf("[%d] Cleaning up...\n", rank);
     // free_predictions(tree_votes, n_trees);
     // free_predictions(forest_predictions, test_set->rows);
     free_subset_dataset(train_set);
     free_subset_dataset(test_set);
+    free_subset_dataset(train_subset);
     free_dataset(dt);
 
     MPI_Finalize();
